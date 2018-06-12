5a96d27394c01ae2ac9a33e820523c79abc19b4c
moeketsane SEFAKO <msefako@c3r7s7.wethinkcode.co.za>
2018-06-12 19:26:32 +0200

    2ndcommit

--- get_next_line.c
+++ get_next_line.c
@@ -1,36 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msefako <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/12 13:28:09 by msefako           #+#    #+#             */
+/*   Updated: 2018/06/12 19:26:01 by msefako          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft/libft.h"
 #include "get_next_line.h"
+#include <time.h>
+#include <fcntl.h>
 
 int			get_next_line(const int fd, char **line)
 {
-	char *buff[BUFF_SIZE];
-	int str;
+	char	*buff;
+	int		str;
+	int		i;
 
-	while (string)
+	buff = NULL;
+	i = 0;
+	printf("B1\n");
+	while (fd)
 	{
-		str = read(fd, *buff, BUFF_SIZE);
-		while(buff)
+	printf("B2\n");
+		str = read(fd, buff, BUFF_SIZE);
+		i = 0;
+		if (str)
 		{
+	printf("B3\n");
+			while (buff)
+			{
+				/*	search for '\n' charecter	*/
+				if (buff[i] == '\n')
+					break ;
+				i++;
+			}
+	printf("B4\n");
+	printf("%d ", i)
 			if(buff[i] == '\n')
 			{
 			/*
 			 *	end of line
-			 *	start new line			
+			 *	start new line
 			*/
-		 		ft_strncat(*line, buff[i], i);
-				printf(**line);
+	printf("B5\n");
+				ft_strncat(*line, buff, i);
+	printf("B6\n");
+				buff[i] = '\0';
+				line++;
 			}
 			else
 			{
 			/*	add buffer to current line	*/
-				ft_strncat(*line, buff, BUFF_SIZE);
+				ft_strncat(*line, buff,BUFF_SIZE);
 			}
-
+			buff++;
+			i++;
+			printf("%d:", i);
 		}
+		else
+			printf("read faiiled\n");
+		break ;
 	}
 	return (**line);
 }
 
+
+
+int	main()
+{
+
+	 clock_t start, end;
+	 double time_spent;
+
+     start = clock();
+	int i;
+	int fd = open("large_file", O_RDONLY);
+	char	**line;
+	line = malloc(100000);
+	*line = malloc(100000);
+	i = 1;
+	int count = 0;
+	printf("A\n");
+	while (count != 783)
+	{
+	printf("A\n");
+		count++;
+		i = get_next_line(fd, line);
+		printf("%d: Buff: {%d}  len: %d [%s]\n",i ,BUFF_SIZE,ft_strlen(*line), *line);
+	}
+	 end = clock();
+
+
+	time_spent = (double)(end - start) / CLOCKS_PER_SEC;
+ 	printf("%f\n",time_spent);
+	return(0);
+}
+/*
 int		main()
 {
 	char *line;
@@ -38,3 +108,4 @@ int		main()
 	get_next_line(fd, &line);
 	return (0);
 }
+*/
