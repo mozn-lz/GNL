b5540ccbefe0159fbe51e5e5292977844ad531d3
mozn-lz <mozn.lozn2000@gmail.com>
2018-06-14 08:30:36 +0200

    Got it to print

--- get_next_line.c
+++ get_next_line.c
@@ -14,98 +14,45 @@
 #include "get_next_line.h"
 #include <time.h>
 #include <fcntl.h>
+#include <stdio.h>
 
 int			get_next_line(const int fd, char **line)
 {
 	char	*buff;
-	int		str;
+	int		str_sz;
 	int		i;
 
-	buff = NULL;
+	buff = ft_memalloc(BUFF_SIZE);
 	i = 0;
-	printf("B1\n");
-	while (fd)
+	str_sz = BUFF_SIZE;
+	while (BUFF_SIZE == str_sz)
 	{
-	printf("B2\n");
-		str = read(fd, buff, BUFF_SIZE);
-		i = 0;
-		if (str)
+		str_sz = read(fd, buff, BUFF_SIZE);
+//		i = ft_strlen(buff) - 1;
+		if (buff[ft_strlen(buff) -1] == '\0')
 		{
-	printf("B3\n");
-			while (buff)
-			{
-				/*	search for '\n' charecter	*/
-				if (buff[i] == '\n')
-					break ;
-				i++;
-			}
-	printf("B4\n");
-	printf("%d ", i)
-			if(buff[i] == '\n')
-			{
-			/*
-			 *	end of line
-			 *	start new line
-			*/
-	printf("B5\n");
-				ft_strncat(*line, buff, i);
-	printf("B6\n");
-				buff[i] = '\0';
-				line++;
-			}
-			else
-			{
-			/*	add buffer to current line	*/
-				ft_strncat(*line, buff,BUFF_SIZE);
-			}
-			buff++;
-			i++;
-			printf("%d:", i);
+			ft_bzero((void*)buff, ft_strlen(buff));
+			break ;
 		}
-		else
-			printf("read faiiled\n");
-		break ;
+		ft_putstr(buff);
+//		ft_strncat(line,buff, str_sz);
+		i++;
 	}
-	return (**line);
-}
-
-
-
-int	main()
-{
-
-	 clock_t start, end;
-	 double time_spent;
-
-     start = clock();
-	int i;
-	int fd = open("large_file", O_RDONLY);
-	char	**line;
-	line = malloc(100000);
-	*line = malloc(100000);
-	i = 1;
-	int count = 0;
-	printf("A\n");
-	while (count != 783)
-	{
-	printf("A\n");
-		count++;
-		i = get_next_line(fd, line);
-		printf("%d: Buff: {%d}  len: %d [%s]\n",i ,BUFF_SIZE,ft_strlen(*line), *line);
-	}
-	 end = clock();
-
-
-	time_spent = (double)(end - start) / CLOCKS_PER_SEC;
- 	printf("%f\n",time_spent);
-	return(0);
-}
-/*
-int		main()
-{
-	char *line;
-
-	get_next_line(fd, &line);
+	ft_putchar('\n');
 	return (0);
 }
-*/
+
+int		main(int argc, char **argv)
+{
+	if (argc == 1) return (0);
+		char *line;
+	int fd = open(argv[1], O_RDONLY);
+	while (get_next_line(fd, &line))
+	{
+		printf("a");
+		printf("%s\n", line);
+		free(line);
+	}
+	close(fd);
+	return (0);
+}
