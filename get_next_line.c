fb8d076a63ec7f886204dbffb423fab53ff51ab1
moeketsane SEFAKO <msefako@c3r7s7.wethinkcode.co.za>
2018-06-15 19:24:43 +0200

    l1

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: msefako <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/12 13:28:09 by msefako           #+#    #+#             */
-/*   Updated: 2018/06/12 19:26:01 by msefako          ###   ########.fr       */
+/*   Updated: 2018/06/15 18:16:42 by msefako          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,27 @@
 #include <time.h>
 #include <fcntl.h>
 #include <stdio.h>
-/*
-char	strfintr(const char *s, char c)
+
+char	*ft_strfintr(const char *s, char c)
 {
 	char	*sret;
 	int		i;
 
 	i = 0;
-	if()
-	return (sret)
+	while (s)
+	{
+		if (s[i] == c)
+			if ((sret = strndup(s, i)))
+				return (sret);
+		i++;
+	}
+	return (NULL);
 }
-*/
 
 int			get_next_line(const int fd, char **line)
 {
 	char	*buff;
+	char	*str;
 	int		str_sz;
 	int		i, j;
 
@@ -42,17 +48,30 @@ int			get_next_line(const int fd, char **line)
 		str_sz = read(fd, buff, BUFF_SIZE);
 		while (buff[i])
 		{
-			if (buff[i] == '\n' )
+/*			if (buff[i] == '\n' )
 			{
 				buff[i] = '\0';
 				ft_putchar('\n');
 			}
-			i++;
-			ft_putnbr(j++);
-				ft_putchar(',');
+			*/
+// NEW CONCEPT
+//			ft_putstr(buff);
+			str = ft_strfintr(buff, '\n');
+			if (ft_strcmp(buff, str) == 0)
+			{
+				ft_strncat(*line, buff, ft_strlen(buff));
+			}
+			else
+			{
+				ft_putnbr(j++);
 				ft_putchar('\n');
+//				ft_strncat(*line, str, ft_strlen(str) - 1);
+				str = strndup(buff + ft_strlen(str) , buff - str);
+			}
+//	END CONCEPT
+			i++;
 		}
-		ft_putstr(buff);
+		ft_putstr(*line);
 	}
 	ft_putchar('\n');
 	return (0);
